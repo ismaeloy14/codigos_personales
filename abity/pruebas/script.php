244003c9e4fdee7be7de2a3ee15595f3c13d190e
unknown <angulo.aparicio.ismael@alumnat.copernic.cat>
2020-02-10 17:32:23 +0100

    las palabras se dividen y pasan a otro array para estar más ordenadas

--- abity/pruebas/script.php
+++ abity/pruebas/script.php
@@ -1,5 +1,18 @@
 <?php
 
+/*********** PAGINAS A VER ************/
+
+// https://www.php.net/manual/es/function.trim.php
+
+// https://www.php.net/manual/es/function.file.php
+
+// https://www.php.net/manual/es/function.file-get-contents.php
+
+// https://www.php.net/manual/es/function.file-put-contents.php
+
+// https://www.php.net/manual/es/function.explode.php
+
+
 // Esta variable pilla todo el contenido del archivo txt.
 //$fichero = fopen("documento_de_prueba/documento_prueba.txt", "r");
 $fichero = "documento_de_prueba/documento_prueba.txt";
@@ -13,15 +26,24 @@ $linea = file_get_contents($fichero);
 echo "</br>";
 
 
+$arrayPalabras = array();
+
+
 $palabras = explode("()", $linea);
 $lenght = count($palabras);
 
+
+// REcorre el array y luego pasa la palabra entera al otro array. Asi las palabras quedan "libres".
 for($i=0; $i < $lenght; $i++){
     
     echo trim($palabras[$i]);
+    if ($i != $lenght-1){
+        $arrayPalabras[$i] = $palabras[$i];
+    }
 
 }
-
+echo "</br>";
+var_dump($arrayPalabras);
 
 
 
